84d057d76ec8ea0a2213c9e89c9845f6eca8a7a3
rikardokz <rikardo_kz@live.com.pt>
2020-08-19 17:47:46 +0100

    added styles

---
+++ Site 3 (video player)/script.js
@@ -0,0 +1,5 @@
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const video = document.getElementById("timestamp");
